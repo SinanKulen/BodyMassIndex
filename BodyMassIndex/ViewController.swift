347ac1ac0cdd1d4173046d085cb51e5673dfe63f
Sinan Kulen <sinan.kulen@gmail.com>
2021-10-13 00:30:58 +0300

    Initial Commit

---
+++ BodyMassIndex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BodyMassIndex
+//
+//  Created by Sinan Kulen on 13.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
